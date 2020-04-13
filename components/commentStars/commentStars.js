e17f68841dc4389721670171c7786c6fece03131
baixiyang <xiyangbai@outlook.com>
2020-04-14 00:01:49 +0800

    update-star

---
+++ components/commentStars/commentStars.js
@@ -0,0 +1,26 @@
+// components/commentStars/commentStars.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    maxStars:{
+      type:Number,
+      value:5
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
