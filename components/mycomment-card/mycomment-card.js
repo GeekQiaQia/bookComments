6f35cf7b0ad95615582674469ef4bd7cf04bc256
geekQiaQia <xiyangbai@outlook.com>
2020-04-27 17:43:46 +0800

    'update-my-notes'

---
+++ components/mycomment-card/mycomment-card.js
@@ -0,0 +1,32 @@
+// components/mycomment-card/mycomment-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+	commentArray:{
+			type:Array,
+			value:[]
+		},
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+	onHandleLikeTrigger:function(e){
+		
+		let index=e.target.dataset.index;
+		var myEventDetail = {index} // detail对象，提供给事件监听函数
+		      var myEventOption = {key:"like"} // 触发事件的选项
+		this.triggerEvent('onLikeEvent', myEventDetail, myEventOption)
+	}
+  }
+})
